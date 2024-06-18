bae4e292b5b6c2fa3e524560d19161ed4e49c5eb
Njimifaysa <149830328+Yuraas2001@users.noreply.github.com>
2024-06-18 18:58:48 +0200

    c

--- bp (3).sql
+++ bp (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 17 juin 2024 à 13:58
+-- Généré le : mar. 18 juin 2024 à 16:56
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `bonnespratiques` (
   `Description` text NOT NULL,
   `Etat` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`IDBonnePratique`)
-) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `bonnespratiques`
@@ -58,9 +58,97 @@ INSERT INTO `bonnespratiques` (`IDBonnePratique`, `Description`, `Etat`) VALUES
 (38, 'Mettre ON les alimentations de l\'EBB POS 3 quand nécessaire', 0),
 (39, 'ana_fonc <test> c v', 0),
 (40, 'réception équipement: liste des documents à demander', 0),
-(41, 'expédition équipement: liste des documents à fournir', 1),
-(60, 'Fayza2018!!', 0),
-(63, 'oooooooooooo', 0);
+(41, 'expédition équipement: liste des documents à fournir', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` int NOT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `user` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `profile` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `event_type` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `log_message` text COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `logs`
+--
+
+INSERT INTO `logs` (`id`, `ip_address`, `timestamp`, `user`, `profile`, `event_type`, `log_message`) VALUES
+(1, '::1', '2024-06-04 21:55:24', 'Unknown User', 'Unknown Profile', 'Information', 'Home page accessed.'),
+(2, '::1', '2024-06-04 22:37:22', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(3, '::1', '2024-06-04 22:37:23', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(4, '::1', '2024-06-04 22:37:28', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(5, '::1', '2024-06-04 22:41:44', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(6, '::1', '2024-06-04 22:41:46', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(7, '::1', '2024-06-04 22:41:46', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(8, '::1', '2024-06-04 22:41:47', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(9, '::1', '2024-06-04 22:41:47', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(10, '::1', '2024-06-04 22:42:21', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(11, '::1', '2024-06-04 22:42:22', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(12, '::1', '2024-06-04 22:42:30', 'Unknown User', 'Unknown Profile', 'Information', 'Home page accessed.'),
+(13, '::1', '2024-06-04 22:42:40', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(14, '::1', '2024-06-04 22:45:30', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(15, '::1', '2024-06-04 22:45:31', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(16, '::1', '2024-06-04 22:45:31', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(17, '::1', '2024-06-04 22:45:32', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(18, '::1', '2024-06-04 22:45:37', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(19, '::1', '2024-06-04 22:45:52', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(20, '::1', '2024-06-04 22:49:03', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(21, '::1', '2024-06-04 22:49:03', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(22, '::1', '2024-06-04 23:42:12', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(23, '::1', '2024-06-04 23:42:12', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(24, '::1', '2024-06-04 23:42:13', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(25, '::1', '2024-06-04 23:42:13', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(26, '::1', '2024-06-04 23:42:13', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(27, '::1', '2024-06-04 23:42:13', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(28, '::1', '2024-06-04 23:47:33', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(29, '::1', '2024-06-04 23:47:33', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(30, '::1', '2024-06-04 23:53:15', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(31, '::1', '2024-06-04 23:53:15', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(32, '::1', '2024-06-05 00:06:19', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(33, '::1', '2024-06-05 00:06:19', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(34, '::1', '2024-06-05 00:06:20', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(35, '::1', '2024-06-05 00:06:20', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(36, '::1', '2024-06-05 00:06:20', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(37, '::1', '2024-06-05 00:06:20', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(38, '::1', '2024-06-05 00:06:20', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(39, '::1', '2024-06-05 00:06:20', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(40, '::1', '2024-06-05 00:06:21', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(41, '::1', '2024-06-05 00:06:21', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(42, '::1', '2024-06-05 00:07:10', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(43, '::1', '2024-06-05 00:07:10', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(44, '::1', '2024-06-05 00:07:14', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(45, '::1', '2024-06-05 00:07:14', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(46, '::1', '2024-06-05 00:07:14', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(47, '::1', '2024-06-05 00:07:14', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(48, '::1', '2024-06-05 00:07:15', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(49, '::1', '2024-06-05 00:07:15', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(50, '::1', '2024-06-05 00:07:30', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(51, '::1', '2024-06-05 00:07:30', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(52, '::1', '2024-06-05 00:11:48', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(53, '::1', '2024-06-05 00:11:48', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(54, '::1', '2024-06-05 00:11:50', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(55, '::1', '2024-06-05 00:11:50', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(56, '::1', '2024-06-05 00:11:57', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(57, '::1', '2024-06-05 00:11:57', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(58, '::1', '2024-06-05 00:12:27', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(59, '::1', '2024-06-05 00:12:27', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(60, '::1', '2024-06-05 00:12:29', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(61, '::1', '2024-06-05 00:12:29', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(62, '::1', '2024-06-05 00:12:39', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(63, '::1', '2024-06-05 00:12:39', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(64, '::1', '2024-06-05 00:12:41', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(65, '::1', '2024-06-05 00:12:41', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.'),
+(66, '::1', '2024-06-05 00:12:42', 'Unknown User', 'Unknown Profile', 'Information', 'Entrée de test dans le journal'),
+(67, '::1', '2024-06-05 00:12:42', 'Unknown User', 'Unknown Profile', 'Information', 'Test de la fonction de journalisation.');
 
 -- --------------------------------------------------------
 
@@ -73,7 +161,7 @@ CREATE TABLE IF NOT EXISTS `motscles` (
   `IDMotsCles` int NOT NULL AUTO_INCREMENT,
   `NomMotsCles` varchar(60) NOT NULL,
   PRIMARY KEY (`IDMotsCles`)
-) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `motscles`
@@ -82,7 +170,6 @@ CREATE TABLE IF NOT EXISTS `motscles` (
 INSERT INTO `motscles` (`IDMotsCles`, `NomMotsCles`) VALUES
 (25, 'TOUS'),
 (26, 'TOUS'),
-(27, ''),
 (28, 'PL'),
 (29, 'POS3'),
 (30, 'TOUS'),
@@ -106,18 +193,7 @@ INSERT INTO `motscles` (`IDMotsCles`, `NomMotsCles`) VALUES
 (48, 'POS3'),
 (49, 'TOUS'),
 (50, 'TOUS'),
-(51, 'TOUS'),
-(52, 'test'),
-(53, 'test2'),
-(54, 'khas'),
-(55, 'Blue'),
-(56, 'batata'),
-(57, 'gggg'),
-(58, 'n'),
-(59, 'all'),
-(60, 'hello'),
-(61, 'jijiji'),
-(62, 'oooooo');
+(51, 'TOUS');
 
 -- --------------------------------------------------------
 
@@ -202,7 +278,6 @@ CREATE TABLE IF NOT EXISTS `pratiquemotscles` (
 --
 
 INSERT INTO `pratiquemotscles` (`IDBonnePratique`, `IDMotsCles`) VALUES
-(25, 27),
 (26, 28),
 (26, 29),
 (27, 30),
@@ -226,11 +301,9 @@ INSERT INTO `pratiquemotscles` (`IDBonnePratique`, `IDMotsCles`) VALUES
 (38, 48),
 (39, 49),
 (40, 50),
-(60, 60),
 (23, 25),
 (24, 25),
-(41, 25),
-(63, 62);
+(41, 25);
 
 -- --------------------------------------------------------
 
@@ -267,11 +340,9 @@ INSERT INTO `pratiquephases` (`IDBonnePratique`, `IDPhase`) VALUES
 (38, 35),
 (39, 36),
 (40, 37),
-(60, 20),
 (23, 20),
 (24, 36),
-(41, 37),
-(63, 20);
+(41, 37);
 
 -- --------------------------------------------------------
 
@@ -320,12 +391,12 @@ INSERT INTO `pratiqueprog` (`IDProgramme`, `IDBonnePratique`) VALUES
 (52, 39),
 (53, 39),
 (54, 40),
-(23, 23),
-(24, 23),
 (23, 24),
 (24, 24),
+(24, 41),
 (54, 41),
-(70, 63);
+(23, 23),
+(24, 23);
 
 -- --------------------------------------------------------
 
@@ -338,7 +409,7 @@ CREATE TABLE IF NOT EXISTS `programmes` (
   `IDProgramme` int NOT NULL AUTO_INCREMENT,
   `NomProgramme` varchar(255) NOT NULL,
   PRIMARY KEY (`IDProgramme`)
-) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `programmes`
@@ -380,16 +451,7 @@ INSERT INTO `programmes` (`IDProgramme`, `NomProgramme`) VALUES
 (55, 'GENERIC'),
 (56, 'GENERIC'),
 (57, 'PROG_1'),
-(58, 'PROG_3'),
-(59, ''),
-(60, ''),
-(61, ''),
-(62, ''),
-(63, 'test'),
-(67, 'scwcs'),
-(68, 'PROG_800'),
-(69, 'prog_300'),
-(70, 'sxs');
+(58, 'PROG_3');
 
 -- --------------------------------------------------------
 
@@ -406,17 +468,16 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `NBtentative` tinyint NOT NULL DEFAULT '0',
   `Bloque` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`IDUtilisateur`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`IDUtilisateur`, `NomUtilisateur`, `MotDePasse`, `TypeUtilisateur`, `NBtentative`, `Bloque`) VALUES
-(11, 'admin', '$2y$10$.VC95qOoAGyYTHV68NbSN.0Mr4Jj6LXcV/WGRejNwqgDgm39Z76Tm', 1, 0, 0),
-(44, 'faysa', '$2y$10$fxPPK7owwYItFawIPITVbu4rBCDff71PlF32WEJArKW6XjyNNSc4y', 0, 0, 0),
-(45, 'nasser', '$2y$10$fpz86OssxWu.DfqHMzDc8OMxgA3mmZx7P27V33XLHWLRoIKNCN.1C', 1, 0, 0),
-(48, 'cam', '$2y$10$j7J4uC8bKT.J6v2eUJJqT.JV9aJcXm504f5rqMzhFL1cC6E8p5ugO', 2, 4, 0);
+(59, 'user', '$2y$10$Qfe/brJqGEnMhnD2C.qCEu0j0lDLq822jQn4UDWGQKM5QcBi1DDnu', 0, 0, 0),
+(60, 'super', '$2y$10$tWNOo5/O8W8YyLDxEKtPp.dkd20Pb1ZKgQ5NwvlFUlDH/cA2.TiDK', 2, 0, 0),
+(61, 'admin', '$2y$10$9mvR0uNH4TjSsBuKmv/fQulworylGsx0rn3x5Woup/.WTok7zIk.C', 1, 0, 0);
 
 --
 -- Contraintes pour les tables déchargées
